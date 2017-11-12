3d1e68a19f2f193c5691fd757d0402e1cc87a538
Unknown <aliya24042008@mail.ru>
2017-11-12 17:49:33 +0300

    Решено 3.6.1 - 3

--- studying/main.cpp
+++ studying/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include<cmath>
 
 using namespace std;
 
 int main()
 {
-  cout << "Hello World!" << endl;
+  double a, y;
+  cin>>a;
+  if (abs(a) > 3)
+    y = 3;
+  else
+    if ( abs(a) <= 3 )
+      y = -abs(a) + 3;
+  cout << "y="<< y << endl;
   return 0;
 }
